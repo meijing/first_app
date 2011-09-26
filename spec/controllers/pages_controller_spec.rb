46463f6c4d9393c5b5b743d12f768bba9e50dac4
paula <paula@paula-HP-G62-Notebook-PC.(none)>
2011-09-26 22:38:58 +0200

    added pages controller

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe PagesController do
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_succcess
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+  end 
+
+end
